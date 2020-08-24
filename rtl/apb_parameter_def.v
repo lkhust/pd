219761390fbc46bf3758276aa4971ba458712462
lkhust <lkhust@163.com>
2020-08-24 11:38:09 +0800

    第一版

---
+++ rtl/apb_parameter_def.v
@@ -0,0 +1,15 @@
+
+`define RX_PRE_EDG     192
+`define PRE_BIT_NUM    127
+`define SOP_BIT_NUM    19
+`define CRC_BIT_NUM    39
+`define SOP_HBYTE_NUM  4        // unit is 5bit (totle 20bits)
+`define TX_BIT5_NUM    4
+`define TX_BIT10_NUM   9
+`define RX_BIT5_NUM    4
+`define SYNC_1         5'b11000
+`define SYNC_2         5'b10001
+`define SYNC_3         5'b00110
+`define RST_1          5'b00111
+`define RST_2          5'b11001
+`define EOP            5'b01101
